b7334fa8859299c2b8f05fedf143c3d80d58ee95
Okojie Osemudiame Charles <c.charlesworld@gmail.com>
2016-05-01 04:58:46 +0300

    cat_dbs edited

--- app/models/cat_db.rb
+++ app/models/cat_db.rb
@@ -1,6 +1,6 @@
 class CatDb < ActiveRecord::Base
 	validates :name, presence: true
 	has_attached_file :picture , styles: {thumb: "150x150#"}
-	validates_attachment_content_type :file_name, :content_type => %w(image/jpeg image/jpg image/png)
+	do_not_validate_attachment_file_type :picture
 
 end
